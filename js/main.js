043f516eab5ad85c6a37014a4fea4b0bd2200ec2
Lawrence Kirk <lmkirkiii@gmail.com>
2018-06-17 13:48:52 -0400

    consolidate all code into a js function for an dryer use.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
-console.log("User flipped " + cardOne);
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var checkForMatch = function(){
+		if(cardsInPlay[0]===cardsInPlay[1]){
+			console.log("You found a match!")
+		}
+		else{
+			console.log("Sorry, try again.")
+		}
+	};
+
+var flipCard= function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+		
+};
+
+flipCard(2);
+flipCard(3);
+flipCard(2);
+flipCard(3);
+flipCard(1);
